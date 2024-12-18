e18230bbef84eece4e8560e5ce995d9b8445338a
zlatin-r <zlatinrusev@gmail.com>
2024-12-18 15:36:21 +0100

    Changes Added

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -54,8 +54,12 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petstagram',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
