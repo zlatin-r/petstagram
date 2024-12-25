558e3dcceca9121a21272c6ad3f5af714966b9b3
zlatin-r <zlatinrusev@gmail.com>
2024-12-25 14:36:38 +0100

    Changes Added

--- petstagram/accounts/models/app_user.py
+++ petstagram/accounts/models/app_user.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-AbstractUser
 
 class AppUser(AbstractBaseUser):
     email = models.EmailField(
