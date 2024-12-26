dbe254ece34222bfdb219ee369bc23440ba629e4
zlatin-r <zlatinrusev@gmail.com>
2024-12-26 13:13:18 +0100

    More changes added

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&s!7fcb2=4@%in9nh!^@2zyh2t)3n-jhq_2!ehc5j%io3*(^0%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
