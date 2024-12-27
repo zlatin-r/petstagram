f39969d12b5957ded3d532d746da08f0fc80cbc8
zlatin-r <zlatinrusev@gmail.com>
2024-12-27 15:29:56 +0100

    More changes added

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render
 
+from petstagram.pets.models import Pet
+
 
 # Create your views here.
 
 def pet_add(request):
-    return render(request, template_name='pets/pet-add-page.html')
+    return render(request, 'pets/pet-add-page.html')
 
 
 def pet_details(request, username, pet_slug):
-    return render(request, template_name='pets/pet-details-page.html')
+    pet = Pet.objects.get(slug=pet_slug)
+    all_photos = pet.photo_set.all()
+
+    context = {
+        'pet': pet,
+        'all_photos': all_photos,
+    }
+
+    return render(request, 'pets/pet-details-page.html', context)
 
 
 def pet_edit(request, username, pet_slug):
-    return render(request, template_name='pets/pet-edit-page.html')
+    return render(request, 'pets/pet-edit-page.html')
 
 
 def pet_delete(request, username, pet_slug):
-    return render(request, template_name='pets/pet-delete-page.html')
+    return render(request, 'pets/pet-delete-page.html')
--- petstagram/photos/views.py
+++ petstagram/photos/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
+from petstagram.photos.models import Photo
+
+
 # Create your views here.
 def photo_add(request):
     return render(request, template_name='photos/photo-add-page.html')
 
 def photo_details(request, pk):
-    return render(request, template_name='photos/photo-details-page.html')
+    photo = Photo.objects.get(pk=pk)
+    likes = photo.like_set.all()
+    comments = photo.comment_set.all()
+
+    context = {
+        'photo': photo,
+        'likes': likes,
+        'comments': comments,
+    }
+
+    return render(request, 'photos/photo-details-page.html', context)
 
 def photo_edit(request, pk):
     return render(request, template_name='photos/photo-edit-page.html')
