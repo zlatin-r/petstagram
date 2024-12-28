2f1e036db5370636fe55a0341f8a50951a738c4c
zlatin-r <zlatinrusev@gmail.com>
2024-12-28 10:51:05 +0100

    More changes added

--- petstagram/pets/urls.py
+++ petstagram/pets/urls.py
@@ -5,8 +5,8 @@ from petstagram.pets import views
 urlpatterns = [
     path('add/', views.AddPetView.as_view(), name='pet-add'),
     path('<str:username>/pet/<slug:pet_slug>/', include([
-        path('', views.pet_details, name='pet-details'),
-        path('edit/', views.pet_edit, name='pet-edit'),
-        path('delete/', views.pet_delete, name='pet-delete'),
+        path('', views.PetDetailsView.as_view(), name='pet-details'),
+        path('edit/', views.EditPetView.as_view(), name='pet-edit'),
+        path('delete/', views.DeletePetView.as_view(), name='pet-delete'),
     ]))
 ]
