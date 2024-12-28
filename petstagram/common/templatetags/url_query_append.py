dbd8ced7b7ee46ec293da9c8ac2a7794523d7e22
zlatin-r <zlatinrusev@gmail.com>
2024-12-28 14:59:05 +0100

    More changes added

--- petstagram/common/templatetags/url_query_append.py
+++ petstagram/common/templatetags/url_query_append.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 @register.simple_tag
 def url_query_append_tag(request, field, value):
-    dict_ = request.GET.copy()
-    dict_[field] = value
-
-    return dict_.urlencode()
+    # field = 'page'; value = 2
+    dict_ = request.GET.copy()  # request.GET -> {'pet_name': 'george'} -> ?pet_name=george
+    dict_[field] = value  #  {'pet_name': 'george', 'page': 2}
+    return dict_.urlencode()  # {'pet_name': 'george', 'page': 2} -> pet_name=george&page=2
