9e9b3db7c2c6d96be4bfa7c3d59ac30e9da36545
zlatin-r <zlatinrusev@gmail.com>
2025-01-05 15:37:41 +0100

    More changes added

--- petstagram/accounts/managers.py
+++ petstagram/accounts/managers.py
@@ -8,6 +8,7 @@ class AppUserManager(BaseUserManager):
         def _create_user(self, email, password, **extra_fields):
             if not email:
                 raise ValueError("The given email must be set")
+
             email = self.normalize_email(email)
 
             user = self.model(email=email, **extra_fields)
@@ -19,6 +20,7 @@ class AppUserManager(BaseUserManager):
         def create_user(self, email=None, password=None, **extra_fields):
             extra_fields.setdefault("is_staff", False)
             extra_fields.setdefault("is_superuser", False)
+
             return self._create_user(email, password, **extra_fields)
 
         def create_superuser(self, email=None, password=None, **extra_fields):
