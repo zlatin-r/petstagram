32ee6d6b7bcd43838159cc314c2d5a3f5e1fd1ba
zlatin-r <zlatinrusev@gmail.com>
2025-01-05 15:16:42 +0100

    More changes added

---
+++ petstagram/accounts/models/app_user.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.db import models
+
+
+class AppUser(AbstractBaseUser):
+    email = models.EmailField(
+        unique=True,
+    )
+    is_active = models.BooleanField(
+        default=True
+    )
+    is_staff = models.BooleanField(
+        default=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
