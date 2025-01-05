7b70d2374e3715d9955f2fe9e21952270a872b74
zlatin-r <zlatinrusev@gmail.com>
2025-01-05 16:36:08 +0100

    More changes added

--- petstagram/accounts/views.py
+++ petstagram/accounts/views.py
@@ -12,7 +12,7 @@ class AppUserRegisterView(CreateView):
     model = UserModel
     form_class = AppUserCreationForm
     template_name = 'accounts/register-page.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('index')
 
     def form_valid(self, form):
         response = super().form_valid(form)
