71894c3f237ba66fc1b861331d6da3d7101b9f71
zlatin-r <zlatinrusev@gmail.com>
2025-01-10 16:56:04 +0100

    More changes added

--- petstagram/accounts/models/app_profile.py
+++ petstagram/accounts/models/app_profile.py
@@ -28,3 +28,7 @@ class Profile(models.Model):
         blank=True,
         null=True,
     )
+    def get_full_name(self):
+        if self.first_name and self.last_name:
+            return self.first_name + " " + self.last_name
+        return "Anonymous"
