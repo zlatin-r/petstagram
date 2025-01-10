32a9da40adae0413af9338f220f810dbb20afd46
zlatin-r <zlatinrusev@gmail.com>
2025-01-10 16:21:40 +0100

    More changes added

--- petstagram/common/views.py
+++ petstagram/common/views.py
@@ -66,7 +66,7 @@ def like_functionality(request, photo_id: int):
     if liked_object:
         liked_object.delete()
     else:
-        like = Like(to_photo_id=photo_id)
+        like = Like(to_photo_id=photo_id, user=request.user)
         like.save()
 
     return redirect(request.META['HTTP_REFERER'] + f"#{photo_id}")
@@ -86,6 +86,7 @@ def add_comment(request, photo_id: int):
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.to_photo = photo
+            comment.user = request.user
             comment.save()
 
         return redirect(request.META['HTTP_REFERER'] + f"#{photo_id}")
